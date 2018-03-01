02837379539de23e72e9dbd8dd297f9a24be2e83
Bu Sun Kim <busunkim96@gmail.com>
2018-02-28 17:07:26 -0700

    adding assignment 6

---
+++ assignment6/dialer.js
@@ -0,0 +1,52 @@
+
+$("#dialer_1").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "1";
+})
+
+$("#dialer_2").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "2";
+})
+
+$("#dialer_3").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "3";
+})
+
+$("#dialer_4").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "4";
+})
+
+$("#dialer_5").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "5";
+})
+
+$("#dialer_6").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "6";
+})
+
+$("#dialer_7").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "7";
+})
+
+$("#dialer_8").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "8";
+})
+
+$("#dialer_9").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "9";
+})
+
+$("#dialer_star").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value.concat("*");
+})
+
+$("#dialer_0").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value + "0";
+})
+
+$("#dialer_pound").click(function(){
+  document.getElementById('dialer').value = document.getElementById('dialer').value.concat("#");
+})
+
+$("#dialer_clear").click(function(){
+  document.getElementById('dialer').value = "";
+})
